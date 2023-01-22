51291047318faba7475f1387537b98829abcba3c
Rayncor <sowa332@gmail.com>
2023-01-22 11:37:41 -0800

    added adduser refetch

--- client/src/components/DisplayData.js
+++ client/src/components/DisplayData.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useQuery, gql, useLazyQuery } from '@apollo/client';
+import { useQuery, gql, useLazyQuery, useMutation } from '@apollo/client';
 import { useState } from 'react';
 
 const QUERY_ALL_USERS = gql`
@@ -37,12 +37,27 @@ const QUERY_ONE_FILM = gql`
 }
 `
 
+const CREATE_USER_MUTATION = gql`
+    mutation CreateUser($input: CreateUserInput!) {
+        createUser(input: $input) {
+        id
+        name
+        }
+    }
+`
+
 export const DisplayData = () => {
-    const { data, error, loading } = useQuery(QUERY_ALL_USERS);
+    const { data, error, loading, refetch } = useQuery(QUERY_ALL_USERS);
     const { data: filmData } = useQuery(QUERY_ALL_FILMS);
     const [grabMovie, { data: movieSearchData, error: movieError }] = useLazyQuery(QUERY_ONE_FILM);
     const [movieSearch, setMovieSearch] = useState('');
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [username, setUserName] = useState('');
+    const [age, setAge] = useState(0);
+    const [nationality, setNationality] = useState('');
 
+    const [createUser] = useMutation(CREATE_USER_MUTATION);
 
 
     if (loading) {
@@ -68,7 +83,16 @@ export const DisplayData = () => {
 
 
     return (
-        <div>{data && data.users.map((e) => { return <div><h1>Name: {e.name} Username: {e.username} Email: {e.email} Age: {e.age} Nationality: {e.nationality} </h1></div> })}
+        <div>
+            <div>
+                <button onClick={() => { createUser({ variables: { input: { name, username, email, age: Number(age), nationality } } }); refetch() }}>Create New User</button>
+                <input placeholder='name' onChange={(e) => { setName(e.target.value) }} />
+                <input placeholder='email' onChange={(e) => { setEmail(e.target.value) }} />
+                <input placeholder='username' onChange={(e) => { setUserName(e.target.value) }} />
+                <input placeholder='age' onChange={(e) => { setAge(e.target.value) }} />
+                <input placeholder='nationality' onChange={(e) => { setNationality(e.target.value.toUpperCase()) }} />
+            </div>
+            {data && data.users.map((e) => { return <div><h1>Name: {e.name} Username: {e.username} Email: {e.email} Age: {e.age} Nationality: {e.nationality} </h1></div> })}
             <div>
                 <h2>{filmData && filmData.films.map((e) => { return <div>Title: {e.title} Year: {e.year} Director: {e.director} Rated R? {e.ratedR === true ? <h2>This film is rated R.</h2> : <h2>For all audiences.</h2>} </div> })}</h2>
             </div>
