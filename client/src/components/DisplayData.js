590a69580f8aa9c27207d07678fffa82a1ef4f30
Rayncor <sowa332@gmail.com>
2023-01-22 10:49:07 -0800

    added error handling for movie input search

--- client/src/components/DisplayData.js
+++ client/src/components/DisplayData.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, gql, useLazyQuery } from '@apollo/client';
+import { useState } from 'react';
 
 const QUERY_ALL_USERS = gql`
     query getAllUsers {
@@ -25,21 +26,44 @@ const QUERY_ALL_FILMS = gql`
 }
 `
 
+const QUERY_ONE_FILM = gql`
+        query GetAFilm($title: String!) {
+            film(title: $title){
+            title
+            director
+            year
+            ratedR
+        }
+}
+`
+
 export const DisplayData = () => {
     const { data, error, loading } = useQuery(QUERY_ALL_USERS);
     const { data: filmData } = useQuery(QUERY_ALL_FILMS);
+    const [grabMovie, { data: movieSearchData, error: movieError }] = useLazyQuery(QUERY_ONE_FILM);
+    const [movieSearch, setMovieSearch] = useState('');
+
+
 
     if (loading) {
         <h1>{loading}</h1>
     }
 
     if (data) {
-        console.log(data);
-        console.log(filmData);
+        // console.log(data);
+        // console.log(filmData);
+    }
+
+    if (movieSearchData) {
+        console.log(movieSearchData)
     }
 
     if (error) {
         <h1>Sorry, an error occurred. Please refresh and try again.</h1>
+    }
+
+    if (movieError) {
+        console.log(movieError);
     }
 
 
@@ -47,6 +71,20 @@ export const DisplayData = () => {
         <div>{data && data.users.map((e) => { return <div><h1>Name: {e.name} Username: {e.username} Email: {e.email} Age: {e.age} Nationality: {e.nationality} </h1></div> })}
             <div>
                 <h2>{filmData && filmData.films.map((e) => { return <div>Title: {e.title} Year: {e.year} Director: {e.director} Rated R? {e.ratedR === true ? <h2>This film is rated R.</h2> : <h2>For all audiences.</h2>} </div> })}</h2>
+            </div>
+            <div>
+                <input type="text" placeholder="Movie you want to search" onChange={(e) => { setMovieSearch(e.target.value) }} />
+                <button onClick={() => {
+                    grabMovie({
+                        variables: {
+                            title: movieSearch
+                        }
+                    })
+                }}>Search for movie</button>
+            </div>
+            <div>
+                {movieError && (<h1>Sorry, movie does not exist in database.</h1>)}
+                {movieSearchData && (<h1>Movie searched: {movieSearchData.film.title} {movieSearchData.film.year} {movieSearchData.film.director} {movieSearchData.film.ratedR}</h1>)}
             </div>
         </div>
     )
